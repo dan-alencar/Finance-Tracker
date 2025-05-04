b8e60831a6a93a7f947fc1091b9debd60449d6dd
dan-alencar <daniloalencar@alu.ufc.br>
2025-05-04 20:14:31 -0300

    first commit

---
+++ Finance Tracker/finance_app/main.py
@@ -0,0 +1,12 @@
+from interface.cli import launch_cli
+from core.session import init_app_session
+
+def main():
+    user = init_app_session()
+    if user:
+        launch_cli(user)
+    else:
+        print("❌ Failed to initialize session.")
+
+if __name__ == "__main__":
+    main()
